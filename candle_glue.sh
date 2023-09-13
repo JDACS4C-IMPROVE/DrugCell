907a80926724123c9561f6673ffbe7c4ce909ef7
Andreas Wilke <wilke@anl.gov>
2023-09-13 17:55:43 -0500

    Added IMPROVE_MODEL_DIR

--- candle_glue.sh
+++ candle_glue.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python3 preprocessing_new.py
+### Set env if CANDLE_MODEL is not in same directory as this script
+IMPROVE_MODEL_DIR=${IMPROVE_MODEL_DIR:-$( dirname -- "$0" )}
+
+python3 ${IMPROVE_MODEL_DIR}/preprocessing_new.py
